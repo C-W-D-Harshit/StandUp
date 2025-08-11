013cb29f08231c2b843de1847facaaa30e38828d
C-W-D-Harshit <cwd.harshit911@gmail.com>
2025-08-11 01:43:25 -0400

    Update index.html title and favicon; modify PWA asset configuration to use new icon and remove unused vite.svg file.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
+import { minimal2023Preset as preset } from "@vite-pwa/assets-generator/config";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
     tailwindcss(),
     VitePWA({
       registerType: "autoUpdate",
+      pwaAssets: {
+        preset,
+        image: "public/icon.png",
+      },
     }),
   ],
   resolve: {
